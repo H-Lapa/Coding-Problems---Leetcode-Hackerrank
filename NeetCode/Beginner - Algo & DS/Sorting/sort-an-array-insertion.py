7add0ba4f7afffc2acc1eddddf68f6b9278718e1
Hugo Lapa <Hugolapa888@gmail.com>
2023-08-14 18:10:21 +0100

    insertion sort

---
+++ NeetCode/Beginner - Algo & DS/Sorting/sort-an-array-insertion.py
@@ -0,0 +1,12 @@
+#https://leetcode.com/problems/sort-an-array/
+#insertion sort 
+
+class Solution(object):
+    def sortArray(self, nums):
+        for i in range(len(nums)):
+            j = i - 1
+            while j >= 0 and nums[j+1] < nums[j]:
+                nums[j+1], nums[j] = nums[j], nums[j+1]
+                j -= 1
+
+        return nums
