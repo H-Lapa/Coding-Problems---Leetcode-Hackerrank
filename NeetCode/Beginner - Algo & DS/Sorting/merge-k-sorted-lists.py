ad26ba2189b60ccc231dec28b45712696c83b55b
Hugo Lapa <Hugolapa888@gmail.com>
2023-08-18 16:10:48 +0100

    attempt at merge k sorted list

---
+++ NeetCode/Beginner - Algo & DS/Sorting/merge-k-sorted-lists.py
@@ -0,0 +1,21 @@
+#https://leetcode.com/problems/merge-k-sorted-lists/
+
+
+#hard question
+
+#thoughts on how to tackle this problem:
+class Solution(object):
+    def mergeKLists(self, lists):
+        #lists is multiple linked lists
+
+        #while loop 
+            #see which first value index 0 is the smallest
+            #splice away the index 0  of the linkedlist with the smallest value or move the pointer to next
+            #continue this until the linked list is empty
+            #find a way to deal with being at the end of the linked lists
+
+
+        return res
+        
+
+#first attempt:
