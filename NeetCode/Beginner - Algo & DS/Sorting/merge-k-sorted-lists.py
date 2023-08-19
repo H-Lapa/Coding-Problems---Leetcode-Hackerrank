08cd21ecc40de72ab6667e65771298239f10c4c7
Hugo Lapa <Hugolapa888@gmail.com>
2023-08-19 13:26:32 +0100

    merging linked lists

--- NeetCode/Beginner - Algo & DS/Sorting/merge-k-sorted-lists.py
+++ NeetCode/Beginner - Algo & DS/Sorting/merge-k-sorted-lists.py
@@ -19,3 +19,42 @@ class Solution(object):
         
 
 #first attempt:
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        if not lists:
+            return None
+        
+        res = lists[0]
+        for i in range(1, len(lists)):
+            res = self.merge(res, lists[i])  # Use 'self.' before 'merge'
+        
+        return res
+    
+    def merge(self, list1, list2):  # Notice the 'self' parameter
+        dummy = ListNode(-1)
+        result = dummy  # result will move, but dummy stays at the start
+
+        p1, p2 = list1, list2
+
+        while p1 is not None and p2 is not None:
+            if p1.val < p2.val:
+                result.next = p1  # Connect the node
+                result = result.next  # Move the pointer
+                p1 = p1.next
+            else:
+                result.next = p2
+                result = result.next
+                p2 = p2.next
+
+    
+        if p1 is not None:
+            result.next = p1
+        if p2 is not None:
+            result.next = p2
+
+        return dummy.next  # Skip the dummy node and return the start of the merged list
