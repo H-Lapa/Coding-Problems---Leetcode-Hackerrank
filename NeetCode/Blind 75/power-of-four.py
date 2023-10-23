54995089e383ae8c40cd3045ece1a27ffbb200ed
Hugo Lapa <Hugolapa888@gmail.com>
2023-10-23 08:36:04 +0100

    po4

---
+++ NeetCode/Blind 75/power-of-four.py
@@ -0,0 +1,18 @@
+import math
+class Solution(object):
+    def isPowerOfFour(self, n):
+
+         if n <= 0:
+            return False
+            
+        while n >= 1:
+
+            if n == 1:
+                return True
+                
+            if n % 4 == 1:
+                break
+
+            n = n / 4
+
+        return False
