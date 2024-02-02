e8004c6eba3e338ee704c73759bc1c99f82d1e1d
Hugo Lapa <Hugolapa888@gmail.com>
2024-02-02 11:04:37 +0000

    Implement merge sort algorithm using pointers

--- NeetCode_Algo_Beginners/Sorting/Merge/sort-an-array.py
+++ NeetCode_Algo_Beginners/Sorting/Merge/sort-an-array.py
@@ -33,3 +33,44 @@ class Solution(object):
         return newArr
         
         
+# Now the solution using pointers
+
+#merge - recursive
+
+class Solution(object):
+    def sortArray(self, nums, start=0, end=None):
+        if end is None:
+            end = len(nums)
+
+        if end - start > 1:  # More than one element
+            mid = (start + end) // 2
+            self.sortArray(nums, start, mid)  # Sort the first half
+            self.sortArray(nums, mid, end)    # Sort the second half
+            self.merge(nums, start, mid, end) # Merge the sorted halves
+
+        return nums
+
+        
+    def merge(self, nums, start, mid, end):
+
+        arr1 = nums[start:mid]
+        arr2 = nums[mid:end]
+        newArr = []
+
+        while arr1 and arr2:
+            if arr1[0] <= arr2[0]:
+                newArr.append(arr1.pop(0))
+            else:
+                newArr.append(arr2.pop(0))
+                
+        if not arr1 and arr2:
+            newArr += arr2
+
+        if not arr2 and arr1:
+            newArr += arr1
+
+        # Copy the elements of newArr back into nums
+        for i in range(len(newArr)):
+            nums[start + i] = newArr[i]
+        
+        
